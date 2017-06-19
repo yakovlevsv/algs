27cd7c236661e469c862d512b319f708843b1b06
Svyatoslav Yakovlev <svyatoslav_yakovlev@epam.com>
2017-06-19 13:38:24 +0300

    Find Bugs fixes

--- UnionFind/src/Percolation.java
+++ UnionFind/src/Percolation.java
@@ -48,7 +48,6 @@ public class Percolation {
 	}
 
 	public static void main(String[] args) {
-		Percolation percolation = new Percolation(7);
 	}
 
 	private void initUnionOfTopLevelWithFakeSite(int n) {
